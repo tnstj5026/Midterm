51f0b0e03f877fc08afbac38ff8a0a82ff68cb09
sunseohong <tnstj5026@gmail.com>
2020-10-21 23:26:13 -0700

    finished

--- 15011182-average.c
+++ 15011182-average.c
@@ -4,12 +4,15 @@ int main(void)
 {
         int system, algorithm, network;
         float average;
-
+	
+	// getting numbers
         printf("Input the system programming, algortihm, and network score:");
         scanf("%d%d%d", &system, &algorithm, &network);
 
+	// calculating average
         average = (system + algorithm + network) / 3.0;
 
+	// printing
         printf("The average of the three subjects is %.2lf.\n", average);
 
         return 0;
